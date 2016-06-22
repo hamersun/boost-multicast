0f995117b7e0413b570b6f66e5cbf195e47f37f6
Sun, Sheng Yu, Hamer <Hamer.Sun@barco.com>
2016-06-22 08:33:32 +0800

    adjust format to follow up stroustrup format
      By command: astyle --style=stroustrup sender.cpp/receiver.cpp

--- receiver.cpp
+++ receiver.cpp
@@ -16,100 +16,91 @@
 
 const short multicast_port = 30001;
 
-class receiver
-{
+class receiver {
 public:
-  receiver(boost::asio::io_service& io_service,
-      const boost::asio::ip::address& listen_address,
-      const boost::asio::ip::address& multicast_address)
-    : socket_(io_service), mPacketCount(0), timer_(io_service)
-  {
-    // Create the socket so that multiple may be bound to the same address.
-    boost::asio::ip::udp::endpoint listen_endpoint(
-        listen_address, multicast_port);
-    socket_.open(listen_endpoint.protocol());
-    socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
-    socket_.bind(listen_endpoint);
+    receiver(boost::asio::io_service& io_service,
+             const boost::asio::ip::address& listen_address,
+             const boost::asio::ip::address& multicast_address)
+        : socket_(io_service), mPacketCount(0), timer_(io_service) {
+        // Create the socket so that multiple may be bound to the same address.
+        boost::asio::ip::udp::endpoint listen_endpoint(
+            listen_address, multicast_port);
+        socket_.open(listen_endpoint.protocol());
+        socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+        socket_.bind(listen_endpoint);
 
-    // Join the multicast group.
-    socket_.set_option(
-        boost::asio::ip::multicast::join_group(multicast_address));
+        // Join the multicast group.
+        socket_.set_option(
+            boost::asio::ip::multicast::join_group(multicast_address));
 
-    socket_.async_receive_from(
-        boost::asio::buffer(data_, max_length), sender_endpoint_,
-        boost::bind(&receiver::handle_receive_from, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-    timer_.expires_from_now(boost::posix_time::seconds(20));
-    timer_.async_wait(boost::bind(&receiver::close, this));
-  }
-
-  void handle_receive_from(const boost::system::error_code& error,
-      size_t bytes_recvd)
-  {
-    if (!error)
-    {
-      //timer_.cancel();
-      std::cout.write(data_, bytes_recvd>32?32:bytes_recvd);
-      std::cout << bytes_recvd;
-      std::cout << std::endl;
-      mPacketCount++;
-
-      socket_.async_receive_from(
-          boost::asio::buffer(data_, max_length), sender_endpoint_,
-          boost::bind(&receiver::handle_receive_from, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
-      timer_.expires_from_now(boost::posix_time::seconds(20));
-      timer_.async_wait(boost::bind(&receiver::close, this));
-    } else {
-        std::cerr << error.message() << std::endl;
-        std::cerr << "packet count: " << mPacketCount << std::endl;
+        socket_.async_receive_from(
+            boost::asio::buffer(data_, max_length), sender_endpoint_,
+            boost::bind(&receiver::handle_receive_from, this,
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+        timer_.expires_from_now(boost::posix_time::seconds(20));
+        timer_.async_wait(boost::bind(&receiver::close, this));
     }
-  }
 
-  void close()
-  {
-      if (timer_.expires_at() <= boost::asio::deadline_timer::traits_type::now()) {
-          std::cout << "time out, packet count: " << mPacketCount << std::endl;
-          std::cout << "close socket" << std::endl;
-          socket_.close();
-      }
-  }
+    void handle_receive_from(const boost::system::error_code& error,
+                             size_t bytes_recvd) {
+        if (!error) {
+            //timer_.cancel();
+            std::cout.write(data_, bytes_recvd>32?32:bytes_recvd);
+            std::cout << bytes_recvd;
+            std::cout << std::endl;
+            mPacketCount++;
+
+            socket_.async_receive_from(
+                boost::asio::buffer(data_, max_length), sender_endpoint_,
+                boost::bind(&receiver::handle_receive_from, this,
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
+            timer_.expires_from_now(boost::posix_time::seconds(20));
+            timer_.async_wait(boost::bind(&receiver::close, this));
+        } else {
+            std::cerr << error.message() << std::endl;
+            std::cerr << "packet count: " << mPacketCount << std::endl;
+        }
+    }
+
+    void close() {
+        if (timer_.expires_at() <= boost::asio::deadline_timer::traits_type::now()) {
+            std::cout << "time out, packet count: " << mPacketCount << std::endl;
+            std::cout << "close socket" << std::endl;
+            socket_.close();
+        }
+    }
 
 private:
-  boost::asio::ip::udp::socket socket_;
-  boost::asio::ip::udp::endpoint sender_endpoint_;
-  enum { max_length = 65536 };
-  char data_[max_length];
-  boost::asio::deadline_timer timer_;
-  int mPacketCount;
+    boost::asio::ip::udp::socket socket_;
+    boost::asio::ip::udp::endpoint sender_endpoint_;
+    enum { max_length = 65536 };
+    char data_[max_length];
+    boost::asio::deadline_timer timer_;
+    int mPacketCount;
 };
 
 int main(int argc, char* argv[])
 {
-  try
-  {
-    if (argc != 3)
-    {
-      std::cerr << "Usage: receiver <listen_address> <multicast_address>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 239.255.0.1\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 ff31::8000:1234\n";
-      return 1;
+    try {
+        if (argc != 3) {
+            std::cerr << "Usage: receiver <listen_address> <multicast_address>\n";
+            std::cerr << "  For IPv4, try:\n";
+            std::cerr << "    receiver 0.0.0.0 239.255.0.1\n";
+            std::cerr << "  For IPv6, try:\n";
+            std::cerr << "    receiver 0::0 ff31::8000:1234\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+        receiver r(io_service,
+                   boost::asio::ip::address::from_string(argv[1]),
+                   boost::asio::ip::address::from_string(argv[2]));
+        io_service.run();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
-    boost::asio::io_service io_service;
-    receiver r(io_service,
-        boost::asio::ip::address::from_string(argv[1]),
-        boost::asio::ip::address::from_string(argv[2]));
-    io_service.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+    return 0;
 }
--- sender.cpp
+++ sender.cpp
@@ -18,108 +18,97 @@
 const short multicast_port = 30001;
 const int max_message_count = 120*1000/33;
 
-class sender
-{
+class sender {
 public:
-  sender(boost::asio::io_service& io_service,
-      const boost::asio::ip::address& multicast_address,
-      const int packet_size)
-    : endpoint_(multicast_address, multicast_port),
-      socket_(io_service, endpoint_.protocol()),
-      timer_(io_service),
-      message_count_(0),
-      _packet_size(packet_size)
-  {
-    std::ostringstream os;
-    os << "Message " << message_count_++;
-    message_ = os.str();
+    sender(boost::asio::io_service& io_service,
+           const boost::asio::ip::address& multicast_address,
+           const int packet_size)
+        : endpoint_(multicast_address, multicast_port),
+          socket_(io_service, endpoint_.protocol()),
+          timer_(io_service),
+          message_count_(0),
+          _packet_size(packet_size) {
+        std::ostringstream os;
+        os << "Message " << message_count_++;
+        message_ = os.str();
 //    std::ostream request_stream(&request_);
-    message_buf = (char*)malloc(_packet_size);
-    memset(message_buf, '-', _packet_size);
+        message_buf = (char*)malloc(_packet_size);
+        memset(message_buf, '-', _packet_size);
 //    memcpy(message_buf, message_.c_str(), message_.length());
-    message_.append(message_buf, _packet_size);
+        message_.append(message_buf, _packet_size);
 //    request_stream.write(message_buf, 200*1024);
 //    boost::asio::streambuf::mutable_buffers_type mutableBuffer =
 //              request_.prepare(200*1024);
 
-    socket_.async_send_to(
-        boost::asio::buffer(message_), endpoint_,
-        boost::bind(&sender::handle_send_to, this,
-          boost::asio::placeholders::error));
-  }
-
-  void handle_send_to(const boost::system::error_code& error)
-  {
-    if (!error && message_count_ < max_message_count)
-    {
-      timer_.expires_from_now(boost::posix_time::milliseconds(33));
-      timer_.async_wait(
-          boost::bind(&sender::handle_timeout, this,
-            boost::asio::placeholders::error));
+        socket_.async_send_to(
+            boost::asio::buffer(message_), endpoint_,
+            boost::bind(&sender::handle_send_to, this,
+                        boost::asio::placeholders::error));
     }
-    else {
-      std::cerr << error.message() << std::endl;
-    }
-  }
 
-  void handle_timeout(const boost::system::error_code& error)
-  {
-    if (!error)
-    {
-      std::ostringstream os;
-      os << "Message " << message_count_++;
-      message_ = os.str();
+    void handle_send_to(const boost::system::error_code& error) {
+        if (!error && message_count_ < max_message_count) {
+            timer_.expires_from_now(boost::posix_time::milliseconds(33));
+            timer_.async_wait(
+                boost::bind(&sender::handle_timeout, this,
+                            boost::asio::placeholders::error));
+        } else {
+            std::cerr << error.message() << std::endl;
+        }
+    }
+
+    void handle_timeout(const boost::system::error_code& error) {
+        if (!error) {
+            std::ostringstream os;
+            os << "Message " << message_count_++;
+            message_ = os.str();
 //      std::ostream request_stream(&request_);
 //      memcpy(message_buf, message_.c_str(), message_.length());
 //      request_stream.write(message_buf, 200*1024);
 //      boost::asio::streambuf::mutable_buffers_type mutableBuffer =
 //            request_.prepare(200*1024);
-      message_.append(message_buf, _packet_size);
+            message_.append(message_buf, _packet_size);
 
-      socket_.async_send_to(
-          boost::asio::buffer(message_), endpoint_,
-          boost::bind(&sender::handle_send_to, this,
-            boost::asio::placeholders::error));
+            socket_.async_send_to(
+                boost::asio::buffer(message_), endpoint_,
+                boost::bind(&sender::handle_send_to, this,
+                            boost::asio::placeholders::error));
+        }
     }
-  }
 
 private:
-  boost::asio::ip::udp::endpoint endpoint_;
-  boost::asio::ip::udp::socket socket_;
-  boost::asio::deadline_timer timer_;
-  int message_count_;
-  std::string message_;
-  int _packet_size;
-  char *message_buf;
+    boost::asio::ip::udp::endpoint endpoint_;
+    boost::asio::ip::udp::socket socket_;
+    boost::asio::deadline_timer timer_;
+    int message_count_;
+    std::string message_;
+    int _packet_size;
+    char *message_buf;
 //  boost::asio::streambuf request_;
 };
 
 int main(int argc, char* argv[])
 {
-  try
-  {
-    if (argc > 3 || argc < 2)
-    {
-      std::cerr << "Usage: sender <multicast_address> [packet_size]\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    sender 239.255.0.1 1024\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    sender ff31::8000:1234 1024\n";
-      return 1;
-    }
-    int packet_size = 1024;
-    if (argc == 3) {
-        packet_size = atoi(argv[2]);
+    try {
+        if (argc > 3 || argc < 2) {
+            std::cerr << "Usage: sender <multicast_address> [packet_size]\n";
+            std::cerr << "  For IPv4, try:\n";
+            std::cerr << "    sender 239.255.0.1 1024\n";
+            std::cerr << "  For IPv6, try:\n";
+            std::cerr << "    sender ff31::8000:1234 1024\n";
+            return 1;
+        }
+        int packet_size = 1024;
+        if (argc == 3) {
+            packet_size = atoi(argv[2]);
+        }
+
+        boost::asio::io_service io_service;
+        sender s(io_service, boost::asio::ip::address::from_string(argv[1]), packet_size);
+        io_service.run();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
-    boost::asio::io_service io_service;
-    sender s(io_service, boost::asio::ip::address::from_string(argv[1]), packet_size);
-    io_service.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+    return 0;
 }
